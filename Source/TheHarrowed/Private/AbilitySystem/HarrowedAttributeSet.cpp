9f2d5738a86fdeaf6cddd9b773bcfa90daedb2a4
Terry Gunderson <tgunderson25@gmail.com>
2023-08-07 22:50:05 -0500

    Adding initial Attributes

--- Source/TheHarrowed/Private/AbilitySystem/HarrowedAttributeSet.cpp
+++ Source/TheHarrowed/Private/AbilitySystem/HarrowedAttributeSet.cpp
@@ -3,3 +3,52 @@
 
 #include "AbilitySystem/HarrowedAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UHarrowedAttributeSet::UHarrowedAttributeSet()
+{
+	
+}
+
+void UHarrowedAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UHarrowedAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHarrowedAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHarrowedAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHarrowedAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHarrowedAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHarrowedAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+}
+
+void UHarrowedAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHarrowedAttributeSet, Health, OldHealth);
+}
+
+void UHarrowedAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHarrowedAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UHarrowedAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHarrowedAttributeSet, Mana, OldMana);
+}
+
+void UHarrowedAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHarrowedAttributeSet, MaxMana, OldMaxMana);
+}
+
+void UHarrowedAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHarrowedAttributeSet, Stamina, OldStamina);
+}
+
+void UHarrowedAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHarrowedAttributeSet, MaxStamina, OldMaxStamina);
+}
--- Source/TheHarrowed/Public/AbilitySystem/HarrowedAttributeSet.h
+++ Source/TheHarrowed/Public/AbilitySystem/HarrowedAttributeSet.h
@@ -13,5 +13,44 @@ UCLASS()
 class THEHARROWED_API UHarrowedAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UHarrowedAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital")
+	FGameplayAttributeData MaxMana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category = "Vital")
+	FGameplayAttributeData Stamina;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxStamina, Category = "Vital")
+	FGameplayAttributeData MaxStamina;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldStamina) const;
+
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const;
 };
